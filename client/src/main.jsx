adf98c6cdda8e666e8b110fb5e0f57da04d1b89a
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-08-22 11:19:54 +0200

    mise en place de la route front et récupération des donées du back

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,27 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import AllUsers from "./pages/AllUsers/AllUsers";
+import connexion from "./services/connexion";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "utilisateurs",
+        element: <AllUsers />,
+        loader: async () => {
+          try {
+            const offerTable = await connexion.get("/api/users");
+            return offerTable.data;
+          } catch (error) {
+            throw new Error(error);
+          }
+        },
+      },
+    ],
   },
 ]);
 
---
+++ client/src/pages/AllUsers/AllUsers.jsx
@@ -0,0 +1,7 @@
+import "./AllUsers.css";
+
+function AllUsers() {
+  return <h1>all users</h1>;
+}
+
+export default AllUsers;
